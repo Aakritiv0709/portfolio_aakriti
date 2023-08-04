7839f52dab233db8397c35e7164e2729d8126aa1
aakriti verma <aakritiv0709@gmail.com>
2023-08-04 16:57:19 -0700

    footer Added

--- src/Common Component/Footer Component/Footer.js
+++ src/Common Component/Footer Component/Footer.js
@@ -2,7 +2,9 @@ import React from 'react'
 import FooterStyles from "../Footer Component/Footer.module.css"
 const Footer = () => {
     return (
-        <div className={FooterStyles.heading}>Footer</div>
+        <div className={FooterStyles.heading}>Footer
+            <h1>hello</h1>
+        </div>
     )
 }
 
